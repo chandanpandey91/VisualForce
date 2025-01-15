31bb35f1c5ec3b3ae02cb139dcbd432338e9d82a
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2025-01-15 23:25:50 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <span>
+                Developed by <a href="https://codeforces.com/profile/manish47"  target="_blank" rel="noreferrer">Manish Sah</a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
